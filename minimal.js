6d218289c28eb8653e2621f8dc756fec86dba6db
Taisen.fr (Dev) <vaca@hotmail.fr>
2025-02-14 10:48:25 +0100

    test csp

--- minimal.js
+++ minimal.js
@@ -1,3 +1,15 @@
+document.getElementById("btn-assistance").addEventListener("click", function() {
+    toggleSection('assistance');
+});
+
+document.getElementById("btn-audit").addEventListener("click", function() {
+    toggleSection('audit');
+});
+
+document.getElementById("btn-formation").addEventListener("click", function() {
+    toggleSection('formation');
+});
+
 function toggleSection(sectionId) {
     var sections = document.getElementsByClassName('section');
     for (var i = 0; i < sections.length; i++) {
